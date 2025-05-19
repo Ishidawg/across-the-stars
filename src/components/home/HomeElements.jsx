4c3f753e484de494e251e5a39142fb05d7f0b929
Ishidawg <willianscagol@gmail.com>
2025-05-19 11:23:47 -0300

    fix: using arrow to go back and forward create a stack of audio

--- src/components/home/HomeElements.jsx
+++ src/components/home/HomeElements.jsx
@@ -1,6 +1,6 @@
 import gameLogo from '../../assets/svg/logo.svg';
 import '../../App.css'
-import { NavLink } from 'react-router';
+import { NavLink, useLocation } from 'react-router';
 import useSound from 'use-sound';
 import { useEffect } from 'react';
 
@@ -10,13 +10,22 @@ import startButton from "../../assets/sound/soundeffects/start-button.mp3"
 
 
 export default function HomeElements() {
-
+  const location = useLocation(); // need this to know if route changes, so I can stop music
   const [play, { stop }] = useSound(titleScreenSoundtrack, { volume: 0.2, loop: true });
   const [start] = useSound(startButton, { volume: 0.5 })
 
   useEffect(() => {
     play()
-  }, [play]);
+
+    return () => stop()
+  }, [play, stop]);
+
+  useEffect(() => {
+    play()
+
+    // return means if the componente "unmounts" or smth like return itself
+    return () => stop()
+  }, [location.pathname]);
 
   return (
     <div className='home-container'>
